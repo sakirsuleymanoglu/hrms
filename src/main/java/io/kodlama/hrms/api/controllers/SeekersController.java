892ed288edfbf58031f31cfe78cd430a41919e34
sakirsuleymanoglu <sakirsuleymanoglu@DESKTOP-FL0EAGU>
2021-05-25 20:46:13 +0300

    commit 25 mayıs

---
+++ src/main/java/io/kodlama/hrms/api/controllers/SeekersController.java
@@ -0,0 +1,10 @@
+package io.kodlama.hrms.api.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/seekers/")
+public class SeekersController {
+
+}
